1802e7bebba920024d4f6cd392b5cc560a0a6693
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-12-11 23:48:33 +0000

    Implement basic template loading

--- example1/templates/router.v
+++ example1/templates/router.v
@@ -1,6 +1,6 @@
 router #(
 		.FIFO_DEPTH(`FIFO_DEPTH))
-	) r_`ID (
+	) `NAME (
 		.rx_req(router_rx_req[`ID]),
 		.rx_ack(router_rx_ack[`ID]),
 		.rx_dat(router_rx_dat[`ID])
--- goc.py
+++ goc.py
@@ -4,7 +4,6 @@ import json
 import sys
 import os
 
-
 def loadJSON(jsonFile):
 	with open(jsonFile) as fid:
 		graph = json.load(fid)
@@ -16,29 +15,36 @@ def buildVertexIndex(graph):
 	map1 = {key:val for key, val in zip(vertexNames, range(n))}
 	return map1
 
-def checkGraph(graph):
+def checkGraph(graph, file):
+	def fail(str):
+		print str
+		sys.exit(1)
 	if not graph.get("vertices"):
-		yield "no entry with key 'vertices'"
-		raise StopIteration
+		fail("file '%s' has no entry with key 'vertices'" % file)
 	if not graph.get("edges"):
-		yield "no entry with key 'edges'"
-		raise StopIteration
+		fail("file '%s' has no entry with key 'edges'" % file)
 	vertices = [v["name"] for v in graph["vertices"]]
 	if len(vertices) != len(set(vertices)):
-		yield "multiple vertices with the same name"
+		fail("file '%s' has multiple vertices with the same name" % file)
 
-def generate():
-	workDir = "example1"
+def loadTemplates(tempDirFull):
+	templates = {}
+	for file in os.listdir(tempDirFull):
+		fullFile = os.path.join(tempDirFull, file)
+		with open(fullFile) as fid:
+			tempName = file.replace(".v", "")
+			templates[tempName] = fid.read()
+	return templates
+
+def generate(workDirFull = "example1"):
+	tempDir = "templates"
 	graphFile = "graph1.json"
-	graphFileFull = os.path.join(workDir, graphFile)
+	graphFileFull = os.path.join(workDirFull, graphFile)
+	tempDirFull = os.path.join(workDirFull, tempDir)
 	graph = loadJSON(graphFileFull)
-	errors = [err for err in checkGraph(graph)]
-	if errors:
-		print "Found one or more errors while loading '%s':" % graphFile
-		for e in errors:
-			print "  * %s" % e
-		sys.exit(1)
+	checkGraph(graph, graphFileFull)
 	map1 = buildVertexIndex(graph)
-	print json.dumps(map1, indent=4)
+	templates = loadTemplates(tempDirFull)
+	print json.dumps(templates, indent=4)
 
 generate()
